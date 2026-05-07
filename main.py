1238d5772b3fc1cf09bddb0871d0aedf97263955
Tommy Huynh <tommyhuynhbusiness@gmail.com>
2026-05-07 15:11:59 -0400

    Fully fixed Kinaxis scrapper

--- main.py
+++ main.py
@@ -99,16 +99,20 @@ for site in sites:
     #everything is in iframe 
     if company == "Kinaxis":
 
+        #load iframe first, check for 20 seconds until it exists
         iframe = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.ID, "icims_content_iframe"))
         )
 
+        #switch to iframe from main page
         driver.switch_to.frame(iframe)
 
+        #wait until all anchor tags exist inside the iframe
         WebDriverWait(driver, 20).until(
             EC.presence_of_all_elements_located((By.TAG_NAME, "a"))
         )
 
+        #find all elements with /jobs/ or /job/ in the link
         job_links = driver.find_elements(
             By.CSS_SELECTOR,
             "a[href*='/jobs/'][href*='/job']"
@@ -116,15 +120,24 @@ for site in sites:
 
         print("Kinaxis jobs found:", len(job_links))
 
+        keywords = ["intern", "co-op/intern", "co-op", "coop"]
+
+
+        #loop through job links
         for job in job_links:
-            title = job.text.strip()
+            #
+            title = job.text.replace("Title", "").strip()
             link = job.get_attribute("href")
 
-            if title:
+            #use this for the print
+            title_lower = title.lower()
+
+            if any(keyword in title_lower for keyword in keywords):
                 print(title)
                 print(link)
                 print()
 
+        #switch back to main page
         driver.switch_to.default_content()
 
         continue
